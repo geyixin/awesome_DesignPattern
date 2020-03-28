5b2c12d62d14a961008b6a464b68eee2342823a1
geyixin <geyixin777@163.com>
2020-03-28 21:05:36 +0800

    Update simpleFactory.cpp

--- simpleFactory.cpp
+++ simpleFactory.cpp
@@ -1,10 +1,10 @@
 /*
  * @Author: Eashin
  * @Date: 2020-03-28 16:24:27
- * @LastEditTime: 2020-03-28 20:26:00
+ * @LastEditTime: 2020-03-28 21:02:52
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
- * @FilePath: /DesignPattern/simpleFactory.cpp
+ * @FilePath: /awesomme_DesignPattern/simpleFactory.cpp
  */
 
 #include <iostream>
@@ -29,13 +29,13 @@ public:
 
     void getName() {
         if (name == "daju"){
-            printf("我是大橘。");
+            cout << "我是大橘。" << endl;
         }
         else if (name == "bosi"){
-            printf("我是波斯猫。")
+            cout << "我是波斯猫。" << endl;
         }
         else if (name == "mddm"){
-            printf("我是缅甸大猫。")
+            cout << "我是缅甸大猫。" << endl;
         }
     }
 private:
@@ -55,3 +55,77 @@ int main(void)
 
 // 使用 简单工厂模式
 
+/**
+ * @description: 定义一个猫咪的抽象类
+ * @param {type} 
+ * @return: 
+ */
+class Cat2 {
+public:
+    virtual void getName() = 0; // 纯虚函数
+};
+
+class Daju:Cat2{
+public:
+    virtual void getName() {
+        cout << "我是大橘。" << endl;
+    }
+}
+
+class Bosi:Cat2{
+public:
+    virtual void getName() {
+        cout << "我是波斯。" << endl;
+    }
+}
+
+class Mddm:Cat2{
+public:
+    virtual void getName() {
+        cout << "我是缅甸大猫。" << endl;
+    }
+}
+
+/**
+ * @description: 猫咪生产工厂
+ * @param {type} 
+ * @return: 
+ */
+class Factory{
+public:
+    Cat2 * createCat(string name) {
+        if (name == "daju"){
+            return new Daju;
+        }
+        eles if (name == "bosi"){
+            return new Bosi;
+        }
+        else if (name == "mddm"){
+            return new Mddm;
+        }
+    }
+}
+
+int main(void)
+{
+    Factory * factory = new Factory;
+    Cat2 * cat2 = NULL;
+    cat2 = factory->createCat("daju");
+    cat2->getName();
+    delete cat2;
+    cat2 = NULL;
+
+    cat2 = factory->createCat("bosi");
+    cat2->getName();
+    delete cat2;
+    cat2 = NULL;
+
+    cat2 = factory->createCat("mddm");
+    cat2->getName();
+    delete cat2;
+    cat2 = NULL;
+
+    delete factory;
+    factory = NULL;
+    return 0;
+}
